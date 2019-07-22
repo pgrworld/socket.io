676ef640ef750f4ab413faebb026185faa2202e2
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-07-22 13:35:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
+var fs = require('fs');
+
+app.listen(3000);
+console.log("running on port NO:3000");
+
+var clients = {};
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  
+  socket.on('add-user', function(data){
+    console.log("NEW USER CONNECTED");
+    clients[data.username] = {
+      "socket": socket.id
+    };
+  });
+
+
+  socket.on('private-message', function(data){
+    console.log("Sending: " + data.content + " to " + data.username);
+    if (clients[data.username]){
+      io.sockets.connected[clients[data.username].socket].emit("add-message", data);
+    } else {
+      console.log("User does not exist: " + data.username); 
+    }
+  });
+
+  //Removing the socket on disconnect
+  socket.on('disconnect', function() {
+  	for(var name in clients) {
+  		if(clients[name].socket === socket.id) {
+  			delete clients[name];
+  			break;
+  		}
+  	}	
+  })
+
+});
+
+
+
