968847fa6ab77332ad6bb49a6f203f156e2a2e62
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-07-30 14:19:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 var app = require('http').createServer(handler);
 var io = require('socket.io').listen(app);
 var fs = require('fs');
-var mysql = require('mysql');
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "1111",
-  database: "socket"
- });
+var sql = require("./sql.js");
+
 app.listen(3000);
 console.log("running on port NO:3000");
 
@@ -29,50 +24,33 @@ var array = []                     //joining users list
 var joinuser=""                    //username
 var msg=""                         //message
 var touser = ""                    //sending username
-var output =[]
+var output =""
+var fromuser=""
 io.sockets.on('connection', function (socket) {
  
   socket.on('add-user', function(data){    
     array.push(data.username)
-    joinuser = data.username
-    //console.log(array)
+    exports.joinuser = data.username
     clients[data.username] = {
     "socket": socket.id
   };
 
-    var sql = "INSERT INTO socketTABLE (JOINUSER) VALUES ('"+joinuser+"')";
-    con.query(sql, function (err, res) {
-    if (err) throw err;
-    console.log("username inserted");
-    });
-    
-    con.query('select FROMUSER,MSG FROM socketTABLE1 WHERE TOUSER=?', [joinuser], function(err, res){
-      output=JSON.stringify(res)
-   if (err){
-      throw err;
-   }else{
-      console.log(res)
-    }
-   });
-   
+    sql.function1()
+
   }); 
+
 
    socket.on('private-message', function(data){
    console.log(data.text +":" +data.content + " to " + data.username );  
-   fromuser=data.text
-   touser=data.username
-   //touser1.push(touser)
-   //console.log(touser1)
-   msg=data.content
+   exports.fromuser=data.text
+   exports.touser=data.username
+   exports.msg=data.content
 
-    var sql = "INSERT INTO socketTABLE1 (JOINUSERS,FROMUSER,TOUSER,MSG) VALUES ('"+data.text+"','"+data.text+"','"+touser+"','"+msg+"')";
-    con.query(sql, function (err, res) {
-    if (err) throw err;
-    console.log("SQL: JOINUSERS & FROMUSER & TOUSER & MSG inserted");
-    });
+    sql.function2()
+    console.log(sql.output)
 
    if (clients[data.username]){
-   io.sockets.connected[clients[data.username].socket].emit("add-message", data,array,output);
+   io.sockets.connected[clients[data.username].socket].emit("add-message", data,array,sql.output);
      } else {
       console.log("User does not exist: " + data.username); 
      }
@@ -90,6 +68,13 @@ io.sockets.on('connection', function (socket) {
   })
 
 });
+
+
+
+
+
+
+
 
 
 
