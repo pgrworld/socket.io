12afeb7ddfc5a6ca1c9018d3e656023d6eab3295
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-08-12 18:55:48 +0530

    Update chat.js

--- client/chat.js
+++ client/chat.js
@@ -1,5 +1,5 @@
 $(function(){
-  var socket = io.connect('http://localhost:3000');
+  var socket = io.connect('http://localhost:3000')
   var user = ""
   $(".username-form").on("submit", function(){
     
